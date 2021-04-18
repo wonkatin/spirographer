babb3712bff4d744777b34ab6f5e6d1e95297e35
wonkatin <j.drawers@gmail.com>
2021-04-18 16:36:12 -0500

    est random colors in slideshow

--- script.js
+++ script.js
@@ -63,7 +63,44 @@ d3.select('#startrandom').on('click', function(){
 d3.select('#stoprandom').on('click', function(){
     stopSlideshow = true;
 })
+// function for random color
+var randomColor = (function(){
+    var golden_ratio_conjugate = 0.618033988749895;
+    var h = Math.random();
+  
+    var hslToRgb = function (h, s, l){
+        var r, g, b;
+  
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+  
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+  
+        return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
+    };
+    
+    return function(){
+      h += golden_ratio_conjugate;
+      h %= 1;
+      return hslToRgb(h, 0.5, 0.60);
+    };
+  })();
 
+// function for random spirograph 
 function slideshow() {
     svg.selectAll('*').remove()
     R = getRandomNumber(10, (maxSize * .75))
@@ -81,7 +118,8 @@ function slideshow() {
     let computedSpiroArray = getSpiroArray()
     let path = svg.append('path') //this is the path
         .attr('fill', 'none') // not sure if i need this 
-        .style('stroke', color)
+        .style('stroke', randomColor)
+        // .style('fill', randomColor) // this is the best bug by far! **********
         .attr('stroke-width', stroke) // stroke is the color 
         // .attr('d', line(spiroArray))
         .attr('d', line(computedSpiroArray)) // tells path where to draw the line using x & y coordinates 
